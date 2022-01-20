d7568b04f3a71ff909cd55e3a79215fd350e48ee
Emmanuel HEMERIT <emmanuel@hemer.it>
2022-01-20 08:20:43 +0000

    Update aliases

--- alert-aduserlock.ps1
+++ alert-aduserlock.ps1
@@ -3,13 +3,13 @@ $URi = "http://192.168.0.56:3000"
 
 # Extraction du blocage de compte le plus récent
 # note : select -First 1 permet d'augementer significativement les performances en ne sélectionnant qu'une seule ligne
-$lockedLog = Get-EventLog security | where instanceid -EQ 4740 | select -First 1
+$lockedLog = Get-EventLog security | Where-Object instanceid -EQ 4740 | Select-Object -First 1
 # Récupération de l'index de l'événement
 $indexlog = $lockedLog.Index
 # Récupération de la date et l'heure de l'événement
 $timelog = $lockedLog.TimeGenerated
 # Récupération du nom d'utilisateur et de la machine
-$user = Get-Eventlog security | where index -EQ $indexlog | select -First 1| select -ExpandProperty ReplacementStrings | select -First 2
+$user = Get-Eventlog security | Where-Object index -EQ $indexlog | Select-Object -First 1| Select-Object -ExpandProperty ReplacementStrings | Select-Object -First 2
 # Extraction du nom d'utilisateur
 $username = $user.split(" ")[0]
 # Extraction de l'ordinateur
