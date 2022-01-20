1e7e3e7c7d690aee1febd93dc41923f73796d4a7
Emmanuel HEMERIT <emmanuel@hemer.it>
2022-01-20 08:27:37 +0000

    Translate comments

--- alert-aduserlock.ps1
+++ alert-aduserlock.ps1
@@ -1,23 +1,34 @@
-# variable pour la configuration
-$URi = "http://192.168.0.56:3000"
+# FR : variable pour la configuration
+# EN : web server that listens to POST requests
+$URi = "http://127.0.0.1:3000"
 
-# Extraction du blocage de compte le plus récent
-# note : select -First 1 permet d'augementer significativement les performances en ne sélectionnant qu'une seule ligne
+# FR : Extraction du blocage de compte le plus récent
+# FR : note : Select-Object -First 1 permet d'augementer significativement les performances en ne sélectionnant qu'une seule ligne
+# EN : Extraction of the most recent account block
+# EN : note: Select-Object -First 1 allows to significantly increase performance by selecting only one line
 $lockedLog = Get-EventLog security | Where-Object instanceid -EQ 4740 | Select-Object -First 1
-# Récupération de l'index de l'événement
+# FR:  Récupération de l'index de l'événement
+# EN : Get Event index
 $indexlog = $lockedLog.Index
-# Récupération de la date et l'heure de l'événement
+# FR : Récupération de la date et l'heure de l'événement
+# EN : Get the event date and time
 $timelog = $lockedLog.TimeGenerated
-# Récupération du nom d'utilisateur et de la machine
+# FR : Récupération du nom d'utilisateur et de la machine
+# EN : Extract username and computer name
 $user = Get-Eventlog security | Where-Object index -EQ $indexlog | Select-Object -First 1| Select-Object -ExpandProperty ReplacementStrings | Select-Object -First 2
-# Extraction du nom d'utilisateur
+# FR : Extraction du nom d'utilisateur
+# EN : Get username
 $username = $user.split(" ")[0]
-# Extraction de l'ordinateur
+# FR : Extraction de l'ordinateur
+# EN : get computer name
 $computer = $user.split(" ")[1]
-# Recherche et récupération du nom complet de l'utilisateur
+# FR : Recherche et récupération du nom complet de l'utilisateur
+# EN : get user fullname
 $findUser = Get-ADUser -filter "SamAccountName -like '$username'" | Select-Object name
 $fullname = $findUser.name
-# Génération du body de la requête POST
+# FR : Génération du body de la requête POST
+# EN : Generate body of POST request
 $postParams = @{username=$username;fullname=$fullname;time=$timelog;computer=$computer}
-# Envoi de la commande
+# FR : Envoi de la requête
+# EN : send the request
 Invoke-WebRequest -Uri $URi -Method POST -Body $postParams
